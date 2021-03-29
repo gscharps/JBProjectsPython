2599c8b1cd34727f2dd03e904ab544f27ccc0721
gscharps <49407192+gscharps@users.noreply.github.com>
2021-03-29 10:06:26 -0400

    Delete Tic-Tac-Toe directory

--- Tic-Tac-Toe/Problems/Prime numbers/main.py
+++
@@ -1,9 +0,0 @@
-prime_numbers = []
-for i in range(2, 1000):
-    divisible = []
-    for j in range(2,i):
-        divisible.append(i % j == 0 and i / j != 1)
-    if any(divisible):
-        continue
-    else:
-        prime_numbers.append(i)
--- Tic-Tac-Toe/Problems/Simon says/main.py
+++
@@ -1,9 +0,0 @@
-def what_to_do(instructions):
-    return_str = ''
-    if instructions.endswith("Simon says"):
-        return_str = 'I ' + instructions[:instructions.find("Simon says")]
-    elif instructions.startswith("Simon says"):
-        return_str = 'I ' + instructions[11:]
-    else:
-        return_str = "I won't do it!"
-    return return_str
