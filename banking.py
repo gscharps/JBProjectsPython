f516d7db52d4e22c6d8d6046d841e6c61b8ae98d
gscharps <49407192+gscharps@users.noreply.github.com>
2021-04-27 20:54:24 -0400

    Update banking.py
    more progress on part three of project

--- banking.py
+++ banking.py
@@ -1,23 +1,31 @@
 import sqlite3
 import random
 
+
 #set up db
 class Database:
     conn = sqlite3.connect('card.s3db')
     cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0)')
-    def acct_exists(self, Account):
-        #don't forget to commit after query
-        pass
+    def acct_exists(self, acct_num, pin):
+        self.cur.execute('SELECT * FROM card WHERE number=? AND pin=?', (acct_num, pin))
+        if self.cur.fetchone():
+            return True
+        else:
+            return False
     def insert_acct(self, Account):
         #don't forget to commit after query
-        pass
-    def get_balance(self, acct_num, pin):
-        #need to commit somewhere
-        return cur.execute('SELECT balance FROM card WHERE pin=? AND number=?', pin, acct_num)
+        self.cur.execute('INSERT INTO card VALUES (?, ?, ?, ?)', (Account.id, Account.number, Account.pin, Account.balance))
+        self.conn.commit()
+    def get_balance(self, acct_num):
+        self.cur.execute('SELECT balance FROM card WHERE number=?', acct_num)
+        balance = cur.fetchone()
+        return balance
     def close(self):
-        conn.close()
+        self.conn.close()
 
+#cur.execute('SELECT pin FROM card WHERE number=:num', {"num": Account.acct_num})
+#print(cur.fetchone())
 
 class Account:
     def __init__(self, id, number, pin, balance):
@@ -26,15 +34,18 @@ class Account:
         self.pin = pin
         self.balance = balance
 class AccountGenerator:
-    acct_num_generator = 10000000
     id_generator = 0
+    minCardNumNoCheckSum = 400000010000000
+    maxCardNumNoCheckSum = 400000019999999
+    def CreateCardNum(self):
+        randomCardNum = random.randint(self.minCardNumNoCheckSum, self.maxCardNumNoCheckSum)
+        randomCardNum = (randomCardNum * 10) + self.check_sum(str(randomCardNum))
+        return randomCardNum
     def CreateAccount(self):
-        #use random generator to create
-        #account numbers until one is found that is not in database
-
-        #returns an Account object
-        pass
-    def check_sum(cardNumNoCheckSum):
+        cardNum = self.CreateCardNum()
+        newAcct = Account(self.id_generator, cardNum, 1111, 0)
+        return newAcct
+    def check_sum(self, cardNumNoCheckSum):
         digits = []
         for i in cardNumNoCheckSum:
             digits.append(int(i))
@@ -55,78 +66,64 @@ class AccountGenerator:
         else:
             retval = 10 - sum
             return retval
+
 class UserInterface:
     logged_in_user = None
-    def main_menu():
+    def main_menu(self):
         print('\n1. Create an account')
         print('2. Log into account')
         print('0. Exit')
         return int(input())
-    def logged_in_menu():
+    def logged_in_menu(self):
         print('1. Balance')
         print('2. Log out')
         print('0. Exit')
         return int(input())
-    def login(acct_num, pin):
-        retVal = False
-        all_acct_nums = database.keys()
-        if acct_num in all_acct_nums:
-            test_pin = database[acct_num]['pin']
-            if pin == test_pin:
-                retVal = True
-            else:
-                retVal = False
-        return retVal
-    def display_card_info(Acct):
-        print('You card has been created')
+    def login(self, database, acct_num, pin):
+        if (database.acct_exists(acct_num, pin)):
+            return True
+        else:
+            return False
+    def display_card_info(self, Acct):
+        print('\nYou card has been created') 
         print('Your card number:')
-        #cardNumNoCheckSum = 400000000000000 + Account.acct_num
         print(Acct.number)
-        #cardNumWithCheckSum = cardNumNoCheckSum*10 + check_sum(str(cardNumNoCheckSum))
-        #print(str(cardNumWithCheckSum))
-        print('Your card PIN:') 
+        print('Your card PIN:')
         print(Acct.pin)
-        #cur.execute('SELECT pin FROM card WHERE number=:num', {"num": Account.acct_num})
-        #print(cur.fetchone())
 
 UI = UserInterface()
 db = Database()
 choice = UI.main_menu()
+acct_generator = AccountGenerator()
 
 while choice != 0:
     if choice == 1:
-        newAcct = AccountGenerator.CreateAccount()
-        Database.insert_acct(newAcct)
+        newAcct = acct_generator.CreateAccount()
+        while db.acct_exists(newAcct.number, newAcct.pin):
+            newAcct = acct_generator.CreateAccount()
+        db.insert_acct(newAcct)
         UI.display_card_info(newAcct)
+        choice = UI.main_menu()
         continue
-        #if Database.acct_exists(newAcct):
-        #    print('Account already exists')
-        #    choice = main_menu()
-        #    continue
-        #else:
-        #    Database.insert_acct(newAcct)
-        #    print('\nYour card has been created')
-        #    display_card_info(newAcct)
-        #    acct_num_generator += 1
-        #    id_generator += 1
+
     elif choice == 2:
         number = int(input('\nEnter your card number:'))
         pin = int(input('Enter your PIN:'))
-        if login(acct_num, pin):
-            print('\nYou have successfully logged in!')
-            logged_in_choice = logged_in_menu()
-            while logged_in_choice == 1:
-                print('Balance: ' + str(get_balance(acct_num, pin)))
-                logged_in_choice = logged_in_menu()
-            if logged_in_choice == 2:
-                choice = main_menu()
-                continue
-            else:
-                break
+        if UI.login(db, number, pin):
+            print('You have successfully logged in!')
+            logged_in_choice = UI.logged_in_menu()
+            while logged_in_choice != 0:
+                if logged_in_choice == 1:
+                    print('Balance: ' + str(db.get_balance(acct_num)))
+                    logged_in_choice = UI.logged_in_menu()
+                    continue
+                elif logged_in_choice == 2:
+                    print('You have successfully logged out!')
+                    break
         else:
             print('Wrong card number or PIN!')
             logged_in_user = None
-    choice = main_menu()
+    choice = UI.main_menu()
 print('Bye!')
 
 db.close()
